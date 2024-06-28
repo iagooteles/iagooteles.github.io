52b22656c9d7af2fed289b0c52f2a49cddb9da19
Iago Teles <iagooteles@gmail.com>
2024-06-28 12:41:19 -0300

    Adicionado senha forte e update no readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Projeto de Login/Register
 
-Este é um projeto de exemplo para um sistema de login e registro, utilizando React para o front-end e Tailwind CSS para a estilização.
+Este é um projeto de exemplo para um sistema de login e registro, utilizando React para o front-end; Tailwind CSS e CSS para a estilização; ESLint para organização do código; Verificação de senha forte com javascript e utilizando regex; E react-router-dom para a organização de rotas.
+Optei por hospedar o projeto no próprio github, assim como fazer o deploy pelo mesmo, usando GitHub Pages.
+O projeto encontra-se responsivo, sua limitação para telas maiores é de 716px, sendo possível mudá-lo para 375x812 através do devtools => Optei por fazer um modelo também acessível a desktop ou tablet, ao invés de apenas mobile como exemplificado no figma;
 
 ## Tecnologias Utilizadas
 
@@ -13,16 +15,31 @@ Este é um projeto de exemplo para um sistema de login e registro, utilizando Re
 
 - **Registro de Usuário**: Permite que novos usuários criem uma conta.
 - **Login de Usuário**: Permite que usuários existentes façam login.
+- **Navegação entre as páginas**: Permite que usuários naveguem entre as páginas de login e Create Account.
 - **Validação de Formulário**: Validação de campos do formulário para garantir entradas corretas.
-- **Feedback ao Usuário**: Mensagens de erro e sucesso para ações de login/registro.
+- **Página 404**: Página 404 funcionando e ativada caso o usuário tente entrar em alguma url não implementada.
 
 ## Como Executar o Projeto
 
-1.
+1. O projeto já se encontra em deploy, podendo ser acessado pelo link:
+2. O repositório github deste projeto é: https://github.com/iagooteles/login-register-bellosoft
    
 
 ## Observacões
 
-- **Página 404**: Página 404 funcionando e ativada caso o usuário tente entrar em alguma url não implementada.
+- **Strong password**: Senha forte implementada na rota /CreateAccount, existe a verificação de tamanho (tem que ser maior que 8) e se possui ao menos 1 uppercase, 1 lowercase, 1 número e 1 caractere especial.
+- **Funcionalidade**: Até o momento(30/06/2024), os botões funcionais são: 
+    - Ícone de mudar a visibilidade no input 'password', em ambas as páginas;
+    - 'Create Now' na página principal, ou página de login;
+    - 'Login Now' na página de /CreateAccount;
+    - 'Voltar para página de Login.' na página 404;
 
-new
+
+## Futuros Updates
+
+Devido ao gosto por este projeto e por interesse próprio de tê-lo em meu portfólio, pretendo adicionar algumas features futuras como:
+    - Implementação do backend;
+    - Implementação com banco de dados postgres;
+    - Adicionar Página caso o login tenha sido um sucesso;
+    - Block do usuário caso digite a senha errada x vezes;
+    - Verificação se o usuário já existe no banco de dados;
--- src/components/CreateAccountForm/Form.jsx
+++ src/components/CreateAccountForm/Form.jsx
@@ -7,15 +7,34 @@ import { useState } from "react";
 
 export default function Form() {
   const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordStrength, setPasswordStrength] = useState("");
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const checkPasswordStrength = (password) => {
+    const strongPasswordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (password.length < 8) {
+      return "Password must be at least 8 characters long";
+    } else if (!strongPasswordRegex.test(password)) {
+      return "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character";
+    } else {
+      return "Strong password";
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    setPasswordStrength(checkPasswordStrength(newPassword));
+  };
+
   return (
     <div className="form-container-create-account">
       <form action="#" method="POST">
-
         <div className="input-box">
           <div className="input-box-container flex flex-row">
             <img src={emailIcon} alt="email icon" className="svg" />
@@ -55,31 +74,34 @@ export default function Form() {
         </div>
 
         <div className="input-box">
-            <div className="input-box-container flex flex-row">
-                <img src={lockIcon} alt="email icon" className="svg" />
-                <div class="label-input-container flex flex-col">
-                    <label for="email" className="input-label">
-                        Password
-                    </label>
-                    <input
-                        id="email"
-                        name="email"
-                        type={showPassword ? "text" : "password"}
-                        placeholder="Enter your password"
-                        autocomplete="off"
-                        required
-                    />
-                </div>
-                <button type="button" onClick={togglePasswordVisibility} >
-                    <img src={eyeIcon} alt="eye icon" className="svg-eye" />
-                </button>
+          <div className="input-box-container flex flex-row">
+            <img src={lockIcon} alt="lock icon" className="svg" />
+            <div className="label-input-container flex flex-col">
+              <label htmlFor="password" className="input-label">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                autoComplete="off"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+              <small>{passwordStrength}</small>
             </div>
+            <button type="button" onClick={togglePasswordVisibility}>
+              <img src={eyeIcon} alt="eye icon" className="svg-eye" />
+            </button>
+          </div>
         </div>
 
         <div className="login-button-container flex flex-col">
-            <button type="submit" class="login-button-create-account">
-                Create Account
-            </button>
+          <button type="submit" class="login-button-create-account">
+            Create Account
+          </button>
         </div>
       </form>
     </div>
